9aa43dc6c778c45db84b9c44836926fbe5418082
Khong1210 <khongjunquan4@gmail.com>
2026-05-17 00:24:46 +0800

    Update 2026_04_25_142658_create_software_requests_table.php

--- database/migrations/2026_04_25_142658_create_software_requests_table.php
+++ database/migrations/2026_04_25_142658_create_software_requests_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->foreignId('software_id')->constrained('software')->onDelete('cascade');
             $table->string('version');
             $table->integer('status'); 
+
+            
             $table->timestamps();
         });
     }
